556d048d1c1784bb162f6c006f964088da26fb2f
benikraco <49342308+benikraco@users.noreply.github.com>
2022-11-08 16:03:21 -0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tasks',
-        'USER': 'cloud',
+        'USER': 'postgres',
         'PASSWORD': 'cloud',
         'HOST': '192.169.0.21',
         'PORT': '5433',
